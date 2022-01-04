5d30f82441dc1fe794bc6a12a1784475ee77b0a5
Orchid (any/all) Patch <ber@etympress.com>
2022-01-04 15:15:01 -0500

    all the world's a stage...etc

--- war/war/urls.py
+++ war/war/urls.py
@@ -22,7 +22,7 @@ from django.views.generic.base import RedirectView
 from war.war.views import Jade, WarInn, WarEntrance
 
 urlpatterns = [
-    path('', RedirectView.as_view(url='connect')),
+    path('', RedirectView.as_view(url='https://thewarsheadinn.tumblr.com/')),
     path('connect', RedirectView.as_view(url='https://docs.google.com/forms/d/1cI0xz9zYplaaxosRclDTR4B8g0EwcLrJ_qDDOwPihe4/')),
     path('post', RedirectView.as_view(url='https://thewarsheadinn.tumblr.com/')),
     #path('/stage', Jade.as_view(url='stage')),
