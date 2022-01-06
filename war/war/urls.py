e231eb393e300f891a0538868c92b1c70cf4577d
Orchid (any/all) Patch <ber@etympress.com>
2022-01-06 12:26:31 -0500

    Revert "a test of a status page"
    This reverts commit 0dbcd15cd1ccd770f9a4e564b9627eb7494befce.

--- war/war/urls.py
+++ war/war/urls.py
@@ -19,14 +19,13 @@ from django.contrib import admin
 from django.urls import include, path
 #from django.views.generic.base import TemplateView
 from django.views.generic.base import RedirectView
-#from war.war.views import Jade, WarInn, WarEntrance
-from war.war.views import Status
+from war.war.views import Jade, WarInn, WarEntrance
 
 urlpatterns = [
     path('', RedirectView.as_view(url='https://thewarsheadinn.tumblr.com/')),
     path('connect', RedirectView.as_view(url='https://docs.google.com/forms/d/1cI0xz9zYplaaxosRclDTR4B8g0EwcLrJ_qDDOwPihe4/')),
     path('post', RedirectView.as_view(url='https://thewarsheadinn.tumblr.com/')),
-    path('status', Status.as_view(url='stage')),
+    #path('/stage', Jade.as_view(url='stage')),
     path('admin/', admin.site.urls),
     # path('xine', RedirectView.as_view(url=''),
     #path('', Jade.as_view()),
